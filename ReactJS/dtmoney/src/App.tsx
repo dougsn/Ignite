47aff513ecd591770ff0f8febe6ca58dd2406937
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-26 11:19:49 -0300

    Estruturando e estilizando o formulario

--- ReactJS/dtmoney/src/App.tsx
+++ ReactJS/dtmoney/src/App.tsx
@@ -1,12 +1,32 @@
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true); // Modal aberto
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false); // Modal fechado
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
--- ReactJS/dtmoney/src/components/Header/index.tsx
+++ ReactJS/dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void; // Recebera uma função sem retorno
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) { // Pegando a função de abrir o modal via props
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
