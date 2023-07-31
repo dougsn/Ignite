b6b3ee245b6313b43c61f1d069153755a98f15ef
Douglas Nascimento <douglassnascimet@gmail.com>
2023-07-31 16:24:53 -0300

    Configurando o DevTools do reactQuery

--- ReactJS/dashgo/src/pages/_app.tsx
+++ ReactJS/dashgo/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 if (process.env.NODE_ENV === "development") {
   makeServer(); // Para usar a 'api' fake do mirage.
@@ -19,6 +20,8 @@ function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
